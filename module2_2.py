3592e4d9599e7d6d681e09b961a59e9ac924674a
Zheka6388 <zheka6388@gmail.com>
2024-09-07 07:06:40 +0300

    Add files via upload

---
+++ module2_2.py
@@ -0,0 +1,19 @@
+first = 123
+second= 456
+third = 789
+if first == third == second:
+    print(3)
+elif first == second or second== third or first == third:
+    print(2)
+elif first != third != second:
+    print(0)
+
+first = 42
+second= 69
+third = 42
+if first == third == second:
+    print(3)
+elif first == second or second== third or first == third:
+    print(2)
+elif first != third != second:
+    print(0)

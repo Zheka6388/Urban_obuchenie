a273a61a70a7061bf1a01953b94e263fd902ad71
Zheka6388 <zheka6388@gmail.com>
2024-09-07 14:03:22 +0300

    Add files via upload

--- module2_2.py
+++ module2_2.py
@@ -1,19 +1,9 @@
-first = 123
-second= 456
-third = 789
+first = int(input('Введите первое число '))#123 ; 42
+second= int(input('Введите второе число '))#456 ; 69
+third = int(input('Введите третье число '))#789 ; 42
 if first == third == second:
     print(3)
 elif first == second or second== third or first == third:
     print(2)
-elif first != third != second:
-    print(0)
-
-first = 42
-second= 69
-third = 42
-if first == third == second:
-    print(3)
-elif first == second or second== third or first == third:
-    print(2)
-elif first != third != second:
+else:
     print(0)

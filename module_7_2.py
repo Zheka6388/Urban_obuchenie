bad28405fd45533c9b3c25869ac11f843b15e0e7
Zheka6388 <zheka6388@gmail.com>
2024-10-17 17:32:55 +0300

    Add files via upload

--- module_7_2.py
+++ module_7_2.py
@@ -4,15 +4,16 @@ def custom_write(file_name, strings): # Функция записывает ст
         # строки и значением в виде строки из strings.   file_name - название файла для записи
         # strings - список строк для записи
         strings_positions = {}  # Создаем пустой словарь strings_positions
-        num_string = 0
-        file = open(file_name, 'w', encoding='utf-8')
+        num_string = 0 # переменная для нумерации строк
+        file = open(file_name, 'w', encoding='utf-8') 
         for string in strings:
-            start = file.tell()
-            file.write(string + '\n')
-            num_string += 1
-            strings_positions[(num_string, start)] = string
-        file.close()
-        return strings_positions
+            start = file.tell()# Для получения номера байта начала строки используется метод tell() перед записью.
+            file.write(string + '\n') # Записываем строку и переходим на новую строку
+            num_string += 1 # увеличиваем номер строки
+            strings_positions[(num_string, start)] = string # Создаем словарь strings_positions, где ключом будет кортеж
+            # (<номер строки>, <байт начала строки>), а значением - записываемая строка.
+        file.close() # закрыть файл
+        return strings_positions# Возвращаем словарь strings_positions
 
 #Пример выполняемого кода:
 info = [

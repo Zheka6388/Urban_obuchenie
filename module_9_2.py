35f12c078fdfafb16f93c3e2f3a18aed1d5b92f8
Zheka6388 <zheka6388@gmail.com>
2024-11-01 06:23:32 +0500

    Add files via upload

--- module_9_2.py
+++ module_9_2.py
@@ -1,7 +1,7 @@
 first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable'] # список, состоящих из строк
 second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler'] # список, состоящих из строк
 
-first_result = [len(x) for x in first_strings if len(x) > 5] # В переменной first_result: список,
+first_result = [len(x) for x in first_strings if len(x) >= 5] # В переменной first_result: список,
 # созданный при помощи сборки состоящий из длин строк списка first_strings, при условии, что длина строк
 # не менее 5 символов.
 print(first_result)
@@ -9,7 +9,7 @@ second_result = [(x, y) for x in first_strings for y in second_strings if len(x)
 # second_result: список, созданный при помощи сборки состоящий из пар слов(кортежей) одинаковой длины. Каждое
 # слово из списка first_strings должно сравниваться с каждым из second_strings. (два цикла)
 print(second_result)
-third_result = [{x: len(x)} for x in first_strings + second_strings if len(x) % 2 == 0] # переменная
+third_result = {x: len(x) for x in first_strings + second_strings if len(x) % 2 == 0} # переменная
 # third_result: словарь, созданный при помощи сборки, где парой ключ-значение будет строка-длина строки.
 # Значения строк будут перебираться из объединённых вместе списков first_strings и second_strings.
 # Условие записи пары в словарь - чётная длина строки.
